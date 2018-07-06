558f2dc4b8e34e91bb0d96eb3dd7dc67c863e09c
hjc <hjc@fastfish.com>
2018-07-06 11:18:20 +0800

    add consule 集成

---
+++ Dyd.BaseService.TaskManager.Node/SystemRuntime/ConsulServiceItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Dyd.BaseService.TaskManager.Node.SystemRuntime
+{
+    public class ConsulRegisteration
+    {
+        public string Host { get; set; }
+        public string Service { get; set; }
+        public string ServiceId { get; set; }
+        public int Port { get; set; }
+    }
+}
