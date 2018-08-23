aa3c5e57c0044b111a80d9864c30b0e37cc71f44
hjc <hjc@fastfish.com>
2018-08-23 11:38:00 +0800

    fix:内存溢出的问题

--- Dyd.BaseService.TaskManager.Domain/Dal/tb_version_dal.cs
+++ Dyd.BaseService.TaskManager.Domain/Dal/tb_version_dal.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.IO;
+using System.Net.Mime;
 using System.Text;
 using XXF.Db;
 using Dyd.BaseService.TaskManager.Domain.Model;
 using XXF.ProjectTool;
+using XXF.Redis;
 
 namespace Dyd.BaseService.TaskManager.Domain.Dal
 {
@@ -19,18 +22,108 @@ namespace Dyd.BaseService.TaskManager.Domain.Dal
                 ps.Add("@taskid", taskid);
                 ps.Add("@version", version);
                 StringBuilder stringSql = new StringBuilder();
-                stringSql.Append(@"select s.* from tb_version s where s.taskid=@taskid and s.version=@version");
+                stringSql.Append(@"select  s.id,s.taskid ,
+          s.version ,
+          s.versioncreatetime ,
+          s.zipfilename ,
+          s.assemblyversion from tb_version s where s.taskid=@taskid and s.version=@version");
                 DataSet ds = new DataSet();
                SqlToDataSet(ds,PubConn, stringSql.ToString(), ps.ToParameters());
+              
                 if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count>0)
                 {
                     var model= CreateModel(ds.Tables[0].Rows[0]);
+                    FillByte(PubConn,taskid,version, model);
                     ds.Dispose();
                   
                     return model;
                 }
                 return null;
             });
+        }
+
+        public void FillByte(DbConn dbConn,int taskid,int version,tb_version_model model)
+
+        {
+            string sql=$@"select s.zipfile from tb_version s where s.taskid={taskid} and s.version={version}";
+
+            IDbConnection conn = dbConn.GetConnection();
+            SqlCommand cmd=null;
+            SqlDataReader rd = null;
+            MemoryStream memoryStream = null;
+            int bufferSize = 32*1024;   
+            // Size of the BLOB buffer.
+            byte[] outbyte = new byte[bufferSize];  // The BLOB byte[] buffer to be filled by GetBytes.
+            long retval;                            // The bytes returned from GetBytes.
+            long startIndex = 0;                    // The starting position in the BLOB output.
+            try
+            {
+
+                cmd =(SqlCommand) conn.CreateCommand();
+
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = sql;
+                //img= new MemoryStream((byte[])cmd.ExecuteScalar());
+               rd= cmd.ExecuteReader(CommandBehavior.SequentialAccess);
+
+                while (rd.Read())
+                {
+                    // Get the publisher id, which must occur before getting the logo.
+                
+
+                    // Create a file to hold the output.
+                    memoryStream = new MemoryStream();
+                    var bw = new BinaryWriter(memoryStream);
+
+                    // Reset the starting byte for the new BLOB.
+                    startIndex = 0;
+
+                    // Read the bytes into outbyte[] and retain the number of bytes returned.
+                    retval = rd.GetBytes(0, startIndex, outbyte, 0, bufferSize);
+
+                    // Continue reading and writing while there are bytes beyond the size of the buffer.
+                    while (retval == bufferSize)
+                    {
+                        bw.Write(outbyte);
+                        bw.Flush();
+
+                        // Reposition the start index to the end of the last buffer and fill the buffer.
+                        startIndex += bufferSize;
+                        retval = rd.GetBytes(0, startIndex, outbyte, 0, bufferSize);
+                    }
+
+                    // Write the remaining buffer.
+                    if(retval > 0) // if file size can divide to buffer size
+                        bw.Write(outbyte, 0, (int)retval); //original MSDN source had retval-1, a bug
+                    bw.Flush();
+                   // bw.Close();
+                    //fs.Seek(0, SeekOrigin.Begin);
+                    model.zipfile=new byte[memoryStream.Length];
+                    model.zipfile = memoryStream.ToArray();
+                    bw.Close();
+                    // Close the output file.
+                  
+                 
+                    
+                    
+                  
+                }
+                
+                //img.Read(model.zipfile,0,(int)img.Length);
+                
+            }
+            finally
+            {
+                rd?.Close();
+
+                cmd?.Dispose();
+
+                memoryStream?.Close();
+                memoryStream?.Dispose();
+            }
+
+
         }
 
         public void SqlToDataSet(DataSet ds, DbConn dbConn, string sql, List<ProcedureParameter> procedurePar)
