1027af41df06bcad91cdb9709f6d5ead6507e2bd
hjc <hjc@fastfish.com>
2018-09-19 14:46:04 +0800

    fix:FERP-2120,2140

---
+++ UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Consul;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Dyd.BaseService.TaskManager.MonitorTasksTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var client = new ConsulClient(configuration =>
+            {
+                configuration.Address = new Uri("http://10.3.13.58:8011/");
+            });
+
+            var checks = client.Agent.Checks().Result.Response; // client.Agent.Services().Result.Response;
+            foreach (var service in checks.Values)
+            {
+                if (service.Status != HealthStatus.Passing)
+                {
+                    // logger.info("unregister : {}", check.getServiceId());
+                    Console.WriteLine($"unreginer:{service.ServiceID}");
+                    client.Agent.ServiceDeregister(service.ServiceID);
+                }
+            }
+        }
+
+
+
+    }
+}
