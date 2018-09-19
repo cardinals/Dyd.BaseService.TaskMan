212bcfe605b7701455733c72a0c7af768ff63c44
hjc <hjc@fastfish.com>
2018-09-19 14:45:26 +0800

    add  MonitorTaskTests.project

---
+++ Dyd.BaseService.TaskManger.MonitorTaskTests/SendTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using Dyd.BaseService.TaskManager.MonitorTasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Dyd.BaseService.TaskManger.MonitorTaskTests
+{
+    [TestClass]
+    public class SendTests
+    {
+        [TestMethod]
+        public void TestSend()
+        {
+            TaskManageErrorSendTask t = new TaskManageErrorSendTask();
+            t.TestRun();
+        }
+
+    }
+}
