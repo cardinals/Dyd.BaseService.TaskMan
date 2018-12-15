030f383d9b1309d1997e86040985b92c7c4c103b
hjc <catchbug@163.com>
2018-12-15 19:25:02 +0800

    app.config 调整

---
+++ Dyd.BaseService.TaskManager.NodeTests/SystemRuntime/Testa.cs
@@ -0,0 +1,29 @@
+﻿using Dyd.BaseService.TaskManager.Node.SystemRuntime;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dyd.BaseService.TaskManager.NodeTests.SystemRuntime
+{
+    [TestClass]
+    public class Testa
+
+    {
+        public Testa()
+        {
+        }
+
+        [TestMethod]
+        public void aa()
+
+        {
+            ConsulRegisterManger mgr = new ConsulRegisterManger();
+            mgr.Register(new ConsulRegisteration {
+                Host= "10.3.13.64",
+                Service="aa",
+                ServiceId=$"aa_localhost_88",
+                Port=5071
+
+
+            }, "http://10.1.13.56:8011");
+        }
+    }
+}
